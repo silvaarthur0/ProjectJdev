18333ed5b2639533fcf3acb547bff05803c32f92
Arthur Silva <Arthur Silva@Arthur>
2025-07-29 15:55:55 -0300

    JOptionTeste

--- JDEv/src/JOptPane/InputPane.java
+++ JDEv/src/JOptPane/InputPane.java
@@ -5,17 +5,21 @@ import javax.swing.JOptionPane;
 public class InputPane {
 
 	public static void main(String[] args) {
-	String carros =	JOptionPane.showInputDialog("Informer a quantidade de carros?");
-	String pessoas =	JOptionPane.showInputDialog("Informer a quantidade de pessoas?");
-
-	double carroNumero = Double.parseDouble(carros);
-	double pessoaNumero = Double.parseDouble(pessoas);
+		String carros =	JOptionPane.showInputDialog("Informer a quantidade de carros?");// Tela Dialogo
+		String pessoas =	JOptionPane.showInputDialog("Informer a quantidade de pessoas?");// Tela Dialogo
 	
-	int divisao = (int) (carroNumero / pessoaNumero); 
-	
-	double resto = carroNumero % pessoaNumero;
-	
-	JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + "carros e sobrou "+ resto + " carros");
+		double carroNumero = Double.parseDouble(carros);//Converte String
+		double pessoaNumero = Double.parseDouble(pessoas);//Converte String
+		
+		int divisao = (int) (carroNumero / pessoaNumero); 
+		
+		double resto = carroNumero % pessoaNumero;
+		
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado?"); // showConfirmDialog Tela de Confirmação
+		
+		if(resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao + " carros e sobrou "+ resto + " carros");// Janela do Resultado
+		}
 	}
 			
 }
